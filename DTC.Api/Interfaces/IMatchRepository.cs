5c086852d9b57eaf110c8fd832583f8fef4f3bd3
sroeblreiter <seb.roebi@gmail.com>
2026-08-27 13:45:25 +0200

    add MatchRepository

---
+++ DTC.Api/Interfaces/IMatchRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DTC.Api.Interfaces
+{
+    public interface IMatchRepository
+    {
+        Task<IEnumerable<Match>> GetByRoundIdAsync(int roundId);
+        Task<Match?> GetByIdAsync(int id);
+        Task<Match> CreateAsync(Match match);
+        Task<Match> UpdateAsync(Match match);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ DTC.Api/Repositories/MatchRepository.cs
@@ -0,0 +1,32 @@
+﻿using DTC.Api.Interfaces;
+
+namespace DTC.Api.Repositories
+{
+    public class MatchRepository : IMatchRepository
+    {
+        public Task<Match> CreateAsync(Match match)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Match?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Match>> GetByRoundIdAsync(int roundId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Match> UpdateAsync(Match match)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
