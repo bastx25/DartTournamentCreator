1cfd06f7a043c2ce5993fa38cbd55a991425d84c
sroeblreiter <seb.roebi@gmail.com>
2026-08-27 13:58:16 +0200

    add Board DTos

---
+++ DTC.Api/Dtos/Board/BoardDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DTC.Api.Dtos.Board
+{
+    public class BoardDto
+    {
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public int Number { get; set; }
+        public string? Label { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
