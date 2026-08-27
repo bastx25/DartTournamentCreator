223e22b97213364a693351de3e018ac27ef4eec9
sroeblreiter <seb.roebi@gmail.com>
2026-08-27 14:14:38 +0200

    add TournamentRepository

---
+++ DTC.Api/Interfaces/ITournamentRepository.cs
@@ -0,0 +1,13 @@
+﻿using DTC.Api.Models;
+
+namespace DTC.Api.Interfaces
+{
+    public interface ITournamentRepository
+    {
+        Task<IEnumerable<Tournament>> GetAllAsync();
+        Task<Tournament?> GetByIdAsync(int id);
+        Task<Tournament> CreateAsync(Tournament tournament);
+        Task<Tournament> UpdateAsync(Tournament tournament);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ DTC.Api/Repositories/TournamentRepository.cs
@@ -0,0 +1,55 @@
+﻿using DTC.Api.Data;
+using DTC.Api.Interfaces;
+using DTC.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTC.Api.Repositories
+{
+    public class TournamentRepository : ITournamentRepository
+    {
+        private readonly DartDbContext _context;
+
+        public TournamentRepository(DartDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Tournament>> GetAllAsync()
+        {
+            return await _context.Tournaments
+                .Include(t => t.Rounds)
+                .ToListAsync();
+        }
+
+        public async Task<Tournament?> GetByIdAsync(int id)
+        {
+            return await _context.Tournaments
+                .Include(t => t.Rounds)
+                    .ThenInclude(r => r.Matches)
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<Tournament> CreateAsync(Tournament tournament)
+        {
+            await _context.Tournaments.AddAsync(tournament);
+            await _context.SaveChangesAsync();
+            return tournament;
+        }
+
+        public async Task<Tournament> UpdateAsync(Tournament tournament)
+        {
+            await _context.SaveChangesAsync();
+            return tournament;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var tournament = await _context.Tournaments.FindAsync(id);
+            if (tournament == null) return false;
+
+            _context.Tournaments.Remove(tournament);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
