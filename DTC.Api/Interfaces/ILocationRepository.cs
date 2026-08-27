ec9c1b7fde714f29b5050233ba9049e94cd2a79c
sroeblreiter <seb.roebi@gmail.com>
2026-08-27 14:02:40 +0200

    add Location Repository

---
+++ DTC.Api/Interfaces/ILocationRepository.cs
@@ -0,0 +1,13 @@
+﻿using DTC.Api.Models;
+
+namespace DTC.Api.Interfaces
+{
+    public interface ILocationRepository
+    {
+        Task<IEnumerable<Location>> GetAllAsync();
+        Task<Location?> GetByIdAsync(int id);
+        Task<Location> CreateAsync(Location location);
+        Task<Location> UpdateAsync(Location location);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ DTC.Api/Repositories/LocationRepository.cs
@@ -0,0 +1,54 @@
+﻿using DTC.Api.Data;
+using DTC.Api.Interfaces;
+using DTC.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTC.Api.Repositories
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly DartDbContext _context;
+
+        public LocationRepository(DartDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Location>> GetAllAsync()
+        {
+            return await _context.Locations
+                .Include(l => l.Boards)
+                .ToListAsync();
+        }
+
+        public async Task<Location?> GetByIdAsync(int id)
+        {
+            return await _context.Locations
+                .Include(l => l.Boards)
+                .FirstOrDefaultAsync(l => l.Id == id);
+        }
+
+        public async Task<Location> CreateAsync(Location location)
+        {
+            await _context.Locations.AddAsync(location);
+            await _context.SaveChangesAsync();
+            return location;
+        }
+
+        public async Task<Location> UpdateAsync(Location location)
+        {
+            await _context.SaveChangesAsync();
+            return location;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null) return false;
+
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
