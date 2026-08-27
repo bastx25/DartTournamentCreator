62bdf8eaa5765a86f52ed63a11e9c1c29c936180
sroeblreiter <seb.roebi@gmail.com>
2026-08-27 13:56:38 +0200

    add Location Dtos

---
+++ DTC.Api/Dtos/Location/CreateLocationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTC.Api.Dtos.Location
+{
+    public class CreateLocationDto
+    {
+        [Required(ErrorMessage = "Name ist ein Pflichtfeld.")]
+        [StringLength(100, ErrorMessage = "Der Name darf maximal 100 Zeichen lang sein.")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(200, ErrorMessage = "Die Adresse darf maximal 200 Zeichen lang sein.")]
+        public string? Address { get; set; }
+    }
+}
---
+++ DTC.Api/Dtos/Location/LocationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DTC.Api.Dtos.Location
+{
+    public class LocationDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public List<BoardDto> Boards { get; set; } = new();
+    }
+}
