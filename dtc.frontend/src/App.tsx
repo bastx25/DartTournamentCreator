0acb0a6a72b3fec36f243fe0bf68bd1a59ffab4d
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-01 13:58:16 +0200

    add create tournament

--- dtc.frontend/src/App.tsx
+++ dtc.frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/home/HomePage";
+import { CreateTournamentPage } from "./pages/tournament/CreateTournamentPage";
 
 function App() {
-   
-    return (
-        <>
-            <Routes>
-                <Route index element={<HomePage />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="tournaments/create" element={<CreateTournamentPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ dtc.frontend/src/pages/tournament/CreateTournamentPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+
+export function CreateTournamentPage() {
+  return (
+    <>
+      <Header />
+
+      <p>Erstelle ein Turnier</p>
+    </>
+  );
+}
