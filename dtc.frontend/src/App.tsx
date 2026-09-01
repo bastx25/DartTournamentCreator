5be6f823a3fddbbda8fe202f10492cdd5f98b59b
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-01 14:03:11 +0200

    add empty TournamentPage

--- dtc.frontend/src/App.tsx
+++ dtc.frontend/src/App.tsx
@@ -5,12 +5,14 @@ import { CreateTournamentPage } from "./pages/tournament/CreateTournamentPage";
 import { SettingsPage } from "./pages/settings/SettingsPage";
 import { PlayerPage } from "./pages/player/PlayerPage";
 import { DashboardPage } from "./pages/dashboard/DashboardPage";
+import { TournamentPage } from "./pages/tournament/TournamentPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="tournaments" element={<TournamentPage />} />
         <Route path="tournaments/create" element={<CreateTournamentPage />} />
         <Route path="settings" element={<SettingsPage />} />
         <Route path="players" element={<PlayerPage />} />
---
+++ dtc.frontend/src/pages/tournament/TournamentPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+
+export function TournamentPage() {
+  return (
+    <>
+      <Header />
+
+      <p>Turnier Übersicht</p>
+    </>
+  );
+}
