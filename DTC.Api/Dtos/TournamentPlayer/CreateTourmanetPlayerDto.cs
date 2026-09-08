990133b2d3d046c48fa82914de30d048e9544003
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-08 07:46:54 +0200

    add TournamentPlayerDtos and Mapper

---
+++ DTC.Api/Dtos/TournamentPlayer/CreateTourmanetPlayerDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTC.Api.Dtos.TournamentPlayer
+{
+    public class CreateTournamentPlayerDto
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Das Turnier ist ein Pflichtfeld.")]
+        public int TournamentId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Der Spieler ist ein Pflichtfeld.")]
+        public int PlayerId { get; set; }
+    }
+}
---
+++ DTC.Api/Dtos/TournamentPlayer/TourmanetPlayerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DTC.Api.Dtos.TournamentPlayer
+{
+    public class TournamentPlayerDto
+    {
+        public int Id { get; set; }
+
+        public int TournamentId { get; set; }
+
+        public int PlayerId { get; set; }
+    }
+}
