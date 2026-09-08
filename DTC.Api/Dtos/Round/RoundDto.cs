2e99bd3ebd7fbd55bc6ce64b551e50f2b3bd4252
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-08 07:56:16 +0200

    add RoundDtos and Mapper

---
+++ DTC.Api/Dtos/Round/RoundDto.cs
@@ -0,0 +1,23 @@
+﻿using DTC.Api.Enums;
+
+namespace DTC.Api.Dtos.Round
+{
+    public class RoundDto
+    {
+        public int Id { get; set; }
+
+        public int TournamentId { get; set; }
+
+        public int Sequence { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTimeOffset PlannedStart { get; set; }
+
+        public DateTimeOffset? PlannedEnd { get; set; }
+
+        public RoundStatus Status { get; set; }
+
+        public RoundPhase Phase { get; set; }
+    }
+}
---
+++ DTC.Api/Dtos/Round/UpdateRoundDto.cs
@@ -0,0 +1,26 @@
+﻿using DTC.Api.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace DTC.Api.Dtos.Round
+{
+    public class UpdateRoundDto
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Das Turnier ist ein Pflichtfeld.")]
+        public int TournamentId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Die Reihenfolge muss größer als 0 sein.")]
+        public int Sequence { get; set; }
+
+        [StringLength(100, ErrorMessage = "Der Name der Runde darf maximal 100 Zeichen lang sein.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Der geplante Startzeitpunkt ist ein Pflichtfeld.")]
+        public DateTimeOffset PlannedStart { get; set; }
+
+        public DateTimeOffset? PlannedEnd { get; set; }
+
+        public RoundStatus Status { get; set; }
+
+        public RoundPhase Phase { get; set; }
+    }
+}
