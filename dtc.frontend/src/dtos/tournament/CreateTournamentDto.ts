7e4e9211a46dba482a50fed3e8cba321ad45a513
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-08 09:47:32 +0200

    add TournamentDtos

---
+++ dtc.frontend/src/dtos/tournament/CreateTournamentDto.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+import { TournamentMode } from "../../enums/TournamentMode";
+import { TournamentStatus } from "../../enums/TournamentStatus";
+
+export const createTournamentDtoSchema = z.object({
+  name: z.string().min(1).max(100),
+  startDate: z.iso.datetime({ offset: true }),
+  description: z.string().max(1000).nullable(),
+  mode: z.enum(TournamentMode),
+  status: z.enum(TournamentStatus),
+  matchDurationMinutes: z.number().int().min(1).max(1440),
+  breakBetweenMatchesMinutes: z.number().int().min(0).max(1440),
+});
+
+export type CreateTournamentDto = z.infer<typeof createTournamentDtoSchema>;
---
+++ dtc.frontend/src/dtos/tournament/TournamentDto.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+import { TournamentMode } from "../../enums/TournamentMode";
+import { TournamentStatus } from "../../enums/TournamentStatus";
+import { groupDtoSchema } from "../group/GroupDto";
+import { roundDtoSchema } from "../rounds/RoundDto";
+
+export const tournamentDtoSchema = z.object({
+  id: z.number().int(),
+  name: z.string(),
+  startDate: z.iso.datetime({ offset: true }),
+  description: z.string().nullable(),
+  mode: z.enum(TournamentMode),
+  status: z.enum(TournamentStatus),
+  matchDurationMinutes: z.number().int(),
+  breakBetweenMatchesMinutes: z.number().int(),
+  tournamentPlayers: z.array(tournamentPlayerDtoSchema),
+  groups: z.array(groupDtoSchema),
+  rounds: z.array(roundDtoSchema),
+});
+
+export type TournamentDto = z.infer<typeof tournamentDtoSchema>;
