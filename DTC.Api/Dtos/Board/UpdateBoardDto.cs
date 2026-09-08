f0fd27380e82a68716790510a095fe95c5a19080
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-08 07:36:00 +0200

    add BoardDto and Mapper

---
+++ DTC.Api/Dtos/Board/UpdateBoardDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTC.Api.Dtos.Board
+{
+    public class UpdateBoardDto
+    {
+        [Required(ErrorMessage = "Der Standort ist ein Pflichtfeld.")]
+        public int LocationId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Die Board-Nummer muss größer als 0 sein.")]
+        public int Number { get; set; }
+
+        [StringLength(100, ErrorMessage = "Das Label darf maximal 100 Zeichen lang sein.")]
+        public string? Label { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+}
