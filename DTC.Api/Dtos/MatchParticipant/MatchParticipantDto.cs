a634d66dc61f364b5e3259620b0d6525ecc54daf
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-08 07:42:10 +0200

    add MatchParticipantDtos and Mapper

---
+++ DTC.Api/Dtos/MatchParticipant/MatchParticipantDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DTC.Api.Dtos.MatchParticipant
+{
+    public class MatchParticipantDto
+    {
+        public int Id { get; set; }
+
+        public int MatchId { get; set; }
+
+        public int TournamentPlayerId { get; set; }
+
+        public int Score { get; set; }
+
+        public bool IsWinner { get; set; }
+    }
+}
---
+++ DTC.Api/Dtos/MatchParticipant/UpdateMatchParticipantDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTC.Api.Dtos.MatchParticipant
+{
+    public class UpdateMatchParticipantDto
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Das Match ist ein Pflichtfeld.")]
+        public int MatchId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Der Turnierspieler ist ein Pflichtfeld.")]
+        public int TournamentPlayerId { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Der Score darf nicht negativ sein.")]
+        public int Score { get; set; }
+
+        public bool IsWinner { get; set; }
+    }
+}
