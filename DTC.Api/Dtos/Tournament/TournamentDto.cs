ee174d0cb2b54612075ab3eac40675b648f28b70
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-08 07:57:39 +0200

    add TournamentDtos and Mapper

---
+++ DTC.Api/Dtos/Tournament/TournamentDto.cs
@@ -0,0 +1,23 @@
+﻿using DTC.Api.Enums;
+
+namespace DTC.Api.Dtos.Tournament
+{
+    public class TournamentDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public DateTimeOffset StartDate { get; set; }
+
+        public string? Description { get; set; }
+
+        public TournamentMode Mode { get; set; }
+
+        public TournamentStatus Status { get; set; }
+
+        public int MatchDurationMinutes { get; set; }
+
+        public int BreakBetweenMatchesMinutes { get; set; }
+    }
+}
