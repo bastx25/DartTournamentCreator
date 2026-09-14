7f5865fb93f626ac13f414867b14eca85e21a9f5
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-09-14 11:29:14 +0200

    update-availability database

---
+++ DTC.Api/Migrations/20260914092836_addAvailability.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DTC.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAvailability : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Availabilities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Start = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    End = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    MatchParticipantId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Availabilities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Availabilities_MatchParticipants_MatchParticipantId",
+                        column: x => x.MatchParticipantId,
+                        principalTable: "MatchParticipants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Availabilities_MatchParticipantId",
+                table: "Availabilities",
+                column: "MatchParticipantId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Availabilities");
+        }
+    }
+}
